7e9188e4a75a080d545df0f596849596dc23639e
Emeka-Raymond <emekaraymond0@gmail.com>
2025-11-19 11:32:45 +0100

    Registering send email

--- userapi/utils.py
+++ userapi/utils.py
@@ -1,3 +1,16 @@
 from django.core.mail import send_mail
 from django.conf import settings    
 
+def send_email(username,email):
+    subject = 'Welcome to YALE AUTH APP'
+    body = f'''
+               Hello {username},!  your registration was successful.do reach out to us for any assistance.
+                Thank you for choosing YALE AUTH APP.
+            '''
+    send_email(
+        subject,
+        body,
+        settings.EMAIL_HOST_USER,
+        [email],
+        fail_silently=False,    
+    )
