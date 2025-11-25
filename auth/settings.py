56f4a74bb784ed5c0a2aa6dd8964798efb9a88ba
Emeka-Raymond <emekaraymond0@gmail.com>
2025-11-25 02:46:47 +0100

    done with the views and urls

--- auth/settings.py
+++ auth/settings.py
@@ -1,6 +1,8 @@
 import os
 
 from pathlib import Path
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -10,7 +12,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1vw61w3!mct$4nx$uw%=pt^_af%$r!k8bzigbjm2w&q=os^-e('
+# SECRET_KEY = 'django-insecure-1vw61w3!mct$4nx$uw%=pt^_af%$r!k8bzigbjm2w&q=os^-e('
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -115,6 +118,7 @@ STATIC_URL = 'static/'
 MEDIA_URL = '/image/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'image')
 
+# Email conf
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
