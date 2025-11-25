f3860c4a9086f426b42ab506a1de02f7af1de7c5
Emeka-Raymond <emekaraymond0@gmail.com>
2025-11-25 10:51:07 +0100

    NEW

--- auth/settings.py
+++ auth/settings.py
@@ -1,6 +1,7 @@
 import os
 
 from pathlib import Path
+
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -123,7 +124,8 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 
 
 
